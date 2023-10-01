0aa1025a5317bd932dd11d9062524f658d812eaa
rn-santos227 <rn_santos227@yahoo.com>
2023-10-01 21:38:52 +0800

    added create many questions on question module

--- src/store/modules/questions_module.ts
+++ src/store/modules/questions_module.ts
@@ -1,8 +1,41 @@
+import Question from "@/types/Question";
+import api from "@/helpers/api";
+
 import { defineStore } from "pinia";
+import { authenticatedFetch } from "@/services/api";
 
 export const useCourseModule = defineStore("courses", {
   state: () => ({
     isLoading: false as boolean,
     isTableLoading: false,
   }),
+
+  actions: {
+    async createAll(
+      payload: Question[],
+      id: number
+    ): Promise<Question[] | null> {
+      try {
+        this.isLoading = true;
+        const response = await authenticatedFetch(api.QUESTIONS.CREATEALL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            questions: payload,
+            id: id,
+          }),
+        });
+        const data = await response.json();
+        const { questions } = data;
+        return questions;
+      } catch (error) {
+        console.error("Error Module in:", error);
+        return null;
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
 });
