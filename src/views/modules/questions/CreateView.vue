c1b6febc7b39f0ebe9bcbbcb21ab2aacb9dc16dd
rn-santos227 <rn_santos227@yahoo.com>
2023-10-04 23:25:47 +0800

    update adding and removing of additional questions

--- src/views/modules/questions/CreateView.vue
+++ src/views/modules/questions/CreateView.vue
@@ -1,34 +1,80 @@
 <template>
-  <form v-for="(question, index) in questions" :key="index">
-    <v-row class="my-1">
+  <div class="mx-8">
+    <form v-for="(question, index) in questions" :key="index">
+      <v-card class="mb-8" elevation="4">
+        <v-card
+          class="rounded-0 rounded-t mb-6 py-2"
+          color="purple-darken-3"
+          flat
+        >
+          <v-card-title>
+            <v-row>
+              <v-col>
+                <span class="text-h6"> Question # {{ index + 1 }} </span>
+              </v-col>
+              <v-col class="d-flex">
+                <v-spacer />
+                <v-btn
+                  v-if="questions.length > 1"
+                  density="comfortable"
+                  variant="outlined"
+                  icon="mdi-delete-circle-outline"
+                  @click="remove(index)"
+                >
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-title>
+        </v-card>
+        <v-row class="my-1 px-12">
+          <v-col>
+            <v-text-field
+              v-model="question.content"
+              label="Question Content/Instruction"
+              density="compact"
+              variant="outlined"
+            />
+          </v-col>
+          <v-col>
+            <v-select
+              v-model="question.type"
+              :items="types"
+              label="Question Type"
+              density="compact"
+              variant="outlined"
+            />
+          </v-col>
+          <v-col>
+            <v-file-input
+              v-model="question.file"
+              :items="types"
+              label="Question Attachment"
+              density="compact"
+              variant="outlined"
+            />
+          </v-col>
+        </v-row>
+      </v-card>
+    </form>
+    <v-divider class="my-5" />
+    <v-row>
       <v-col>
-        <v-text-field
-          v-model="question.content"
-          label="Question Content/Instruction"
-          density="compact"
-          variant="outlined"
-        />
+        <v-btn
+          class="mb-2"
+          color="purple-darken-3"
+          block
+          @click.prevent="addQuestion"
+        >
+          Add Question
+        </v-btn>
       </v-col>
       <v-col>
-        <v-select
-          v-model="question.type"
-          :items="types"
-          label="Question Type"
-          density="compact"
-          variant="outlined"
-        />
-      </v-col>
-      <v-col>
-        <v-file-input
-          v-model="question.file"
-          :items="types"
-          label="Question Attachment"
-          density="compact"
-          variant="outlined"
-        />
+        <v-btn class="mb-2" color="success" block @click.prevent="submit">
+          Submit
+        </v-btn>
       </v-col>
     </v-row>
-  </form>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -45,4 +91,23 @@ const questions = ref<Question[]>([
     solutions: [],
   },
 ]);
+
+const addQuestion = () => {
+  questions.value.push({
+    content: "",
+    type: "word problem",
+    file: [],
+    options: [],
+    solutions: [],
+  });
+  console.log(questions.value.length);
+};
+
+const remove = (index: number) => {
+  questions.value.splice(index, 1);
+};
+
+const submit = async () => {
+  //
+};
 </script>
