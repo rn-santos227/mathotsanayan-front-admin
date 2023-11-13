4a53013eef52dccd0634e780abf6df6c64f4cfc1
rn-santos227 <rn_santos227@yahoo.com>
2023-11-13 23:13:44 +0800

    added base for system help

---
+++ src/views/help/MainView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-dialog v-model="dialog">
+    <v-sheet
+      class="ma-auto text-center mx-auto"
+      elevation="12"
+      max-width="550"
+      rounded="lg"
+      width="100%"
+    >
+    </v-sheet>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const dialog = ref<boolean>(false);
+</script>
