4ac5feb55d6cf05577e2206a730dc53011b4a210
rn-santos227 <rn_santos227@yahoo.com>
2023-11-20 00:23:43 +0800

    create update form for question

---
+++ src/views/modules/questions/UpdateView.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-list-item @click.prevent>
+    <v-list-item-title class="text-button">
+      <v-icon icon="mdi-update"></v-icon> Update
+    </v-list-item-title>
+    <v-dialog
+      class="ma-auto"
+      persistent
+      v-model="dialog"
+      activator="parent"
+      width="80%"
+    >
+      <v-card height="560">
+        <v-card
+          class="rounded-0 rounded-t mb-6 py-2"
+          color="purple-darken-3"
+          flat
+        >
+          <v-card-title>
+            <v-row>
+              <v-col>
+                <span class="text-h6">
+                  Update Question ID: {{ padLeft(state.id) }}
+                </span>
+              </v-col>
+              <v-col class="d-flex">
+                <v-spacer />
+                <v-btn
+                  density="comfortable"
+                  variant="outlined"
+                  icon="mdi-close"
+                  @click="close"
+                >
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-title>
+        </v-card>
+      </v-card>
+    </v-dialog>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+
+import Question from "@/types/Question";
+import { padLeft } from "@/helpers/utils";
+
+const dialog = ref<boolean>(false);
+
+const props = defineProps<{
+  question: Question;
+  index: number;
+}>();
+
+const state = reactive<Question>({ ...props.question });
+
+const close = () => {
+  dialog.value = !dialog.value;
+};
+</script>
