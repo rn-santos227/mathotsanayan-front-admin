50c2fe060c36d2317a3af9ab1892fd9040f6362e
rn-santos227 <rn_santos227@yahoo.com>
2023-11-21 22:32:29 +0800

    change file array upload

--- src/store/modules/questions_module.ts
+++ src/store/modules/questions_module.ts
@@ -18,40 +18,12 @@ export const useQuestionModule = defineStore("questions", {
         const formData = new FormData();
         formData.append("module", JSON.stringify(module));
         formData.append("questions", JSON.stringify(payload));
-        for (let index_0 = 0; index_0 < payload.length; index_0++) {
-          const question_file = payload[index_0].file;
-          if (question_file) {
-            formData.append(`question-file-${index_0}`, question_file);
+        payload.forEach((item) => {
+          const file = item.file;
+          if (file) {
+            formData.append("question_files", file);
           }
-
-          for (
-            let index_1 = 0;
-            index_1 < payload[index_0].corrects.length;
-            index_1++
-          ) {
-            const solution_file = payload[index_0].corrects[index_1].file;
-            if (solution_file) {
-              formData.append(
-                `question-${index_0}-solution-file-${index_1}`,
-                solution_file
-              );
-            }
-          }
-
-          for (
-            let index_2 = 0;
-            index_2 < payload[index_0].options.length;
-            index_2++
-          ) {
-            const option_file = payload[index_0].corrects[index_2].file;
-            if (option_file) {
-              formData.append(
-                `question-${index_0}-option-file-${index_2}`,
-                option_file
-              );
-            }
-          }
-        }
+        });
         const response = await authenticatedFetch(
           `${api.QUESTIONS.CREATEALL}${module.id}`,
           {
