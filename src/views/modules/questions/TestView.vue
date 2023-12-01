31cb0d3cdd03a5e8b5daa31c20a362035e52a5a5
rn-santos227 <rn_santos227@yahoo.com>
2023-12-01 23:02:17 +0800

    update answers field

--- src/views/modules/questions/TestView.vue
+++ src/views/modules/questions/TestView.vue
@@ -10,7 +10,7 @@
       activator="parent"
       width="50%"
     >
-      <v-card class="question-height">
+      <v-card height="690">
         <v-card
           class="rounded-0 rounded-t mb-6 py-2"
           color="purple-darken-3"
@@ -36,37 +36,57 @@
             </v-row>
           </v-card-title>
         </v-card>
-        <v-card-text>
-          <v-row>
-            <v-col class="mx-4">
-              <div class="text-body-1">{{ props.question.content }}</div>
-            </v-col>
-          </v-row>
-          <v-row v-if="props.question.file">
-            <v-col class="d-flex justify-center align-center">
-              <ImageComponent
-                v-bind:id="props.question.id"
-                v-bind:file="props.question.file"
-                v-bind:height="300"
-                v-bind:width="300"
-                v-model:trigger="dialog"
-              />
-            </v-col>
-          </v-row>
-          <v-row v-if="props.question.type == 'word problem'">
-            <v-col>
-              <v-text-field
-                class="mx-4"
-                v-model.trim="state.content"
-                label="Provide your answer here."
-                density="compact"
-                variant="outlined"
-              />
-            </v-col>
-          </v-row>
-          <v-row v-if="props.question.type == 'multiple selection'"> </v-row>
-          <v-row v-if="props.question.type == 'single correct'"> </v-row>
-        </v-card-text>
+        <form>
+          <v-card-text class="question-height">
+            <v-row>
+              <v-col class="mx-4">
+                <div class="text-body-1">{{ props.question.content }}</div>
+              </v-col>
+            </v-row>
+            <v-row v-if="props.question.file">
+              <v-col class="d-flex justify-center align-center">
+                <ImageComponent
+                  v-bind:id="props.question.id"
+                  v-bind:file="props.question.file"
+                  v-bind:height="350"
+                  v-bind:width="350"
+                  v-model:trigger="dialog"
+                />
+              </v-col>
+            </v-row>
+            <v-row v-if="props.question.type == 'word problem'">
+              <v-col>
+                <v-text-field
+                  class="mx-4"
+                  v-model.trim="state.content"
+                  label="Provide your answer here."
+                  density="compact"
+                  variant="outlined"
+                />
+              </v-col>
+            </v-row>
+            <v-row v-else-if="props.question.type == 'multiple selection'">
+            </v-row>
+            <v-row v-else-if="props.question.type == 'single correct'"> </v-row>
+          </v-card-text>
+          <v-divider class="mb-2 mt-auto" />
+          <v-card-actions class="text-right">
+            <v-row>
+              <v-col>
+                <v-btn
+                  class="mb-1 mr-4"
+                  variant="elevated"
+                  width="200"
+                  dark
+                  color="success"
+                  prepend-icon="mdi-check"
+                >
+                  Submit
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-actions>
+        </form>
       </v-card>
     </v-dialog>
   </v-list-item>
@@ -100,7 +120,7 @@ const close = () => {
 
 <style scoped>
 .question-height {
-  height: calc(100vh - 200px);
+  height: calc(100vh - 400px);
   overflow-y: auto;
 }
 </style>
