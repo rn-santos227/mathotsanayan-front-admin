d02d566d9fc621db003e59a6af401785ac62870c
rn-santos227 <rn_santos227@yahoo.com>
2023-12-28 20:53:13 +0800

    added table in admin module

---
+++ src/views/admins/TableView.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-data-table
+    class="w-100"
+    :items="admins"
+    :headers="headers"
+    :loading="useAdminsModule().isTableLoading"
+    item-value="name"
+  >
+    <template v-slot:item="{ item }">
+      <tr>
+        <td class="text-left">
+          {{ item.name }}
+        </td>
+        <td class="text-left">
+          {{ item.email }}
+        </td>
+        <td class="text-left">
+          {{ item.contact_number }}
+        </td>
+        <td>
+          <v-btn block color="purple-darken-3" variant="outlined">
+            <v-icon size="large">mdi-dots-horizontal</v-icon>
+            <v-menu activator="parent">
+              <v-list density="compact" variant="plain"> </v-list>
+            </v-menu>
+          </v-btn>
+        </td>
+      </tr>
+    </template>
+  </v-data-table>
+  <SuccessDialogComponent ref="success" />
+  <ErrorDialogComponent ref="error" />
+  <LoadingDialogComponent v-bind:activate="useAdminsModule().isLoading" />
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, provide, ref } from "vue";
+import { useAdminsModule } from "@/store";
+
+import SuccessDialogComponent from "@/components/dialogs/SuccessDialogComponent.vue";
+import ErrorDialogComponent from "@/components/dialogs/ErrorDialogComponent.vue";
+import LoadingDialogComponent from "@/components/dialogs/LoadingDialogComponent.vue";
+
+import headers from "@/helpers/headers/header_admins";
+import Admin from "@/types/Admin";
+
+const success = ref({
+  show: (message: string) => {
+    return message;
+  },
+});
+
+const error = ref({
+  show: (message: string) => {
+    return message;
+  },
+});
+
+const adminModule = useAdminsModule();
+const admins = computed<Admin[]>(() => adminModule.getAdmins);
+
+onMounted(async () => {
+  await useAdminsModule().read();
+});
+
+provide("success", success);
+provide("error", error);
+</script>
