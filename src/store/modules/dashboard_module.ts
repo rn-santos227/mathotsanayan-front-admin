a847140bd3fd7bec29a3a42d9ded6f1e6996ef19
rn-santos227 <rn_santos227@yahoo.com>
2023-12-29 23:16:41 +0800

    added changes in dashboard module store

--- src/store/modules/dashboard_module.ts
+++ src/store/modules/dashboard_module.ts
@@ -2,20 +2,26 @@ import api from "@/helpers/api";
 
 import { defineStore } from "pinia";
 import { authenticatedFetch } from "@/services/api";
+import Dashboard from "@/interfaces/Dashboard";
 
 export const useDasboardModule = defineStore("dashboard", {
   state: () => ({
     isLoading: false as boolean,
+    dashboard: {} as Dashboard,
   }),
 
   actions: {
+    setAccounts(dashboard: Dashboard): void {
+      this.dashboard = dashboard;
+    },
+
     async read() {
       try {
         this.isLoading = true;
         const response = await authenticatedFetch(api.DASHBOARD.READ);
         const data = await response.json();
         const { dashboard } = data;
-        console.log(dashboard);
+        this.setAccounts(dashboard);
       } catch (error) {
         console.error("Error Course in:", error);
       } finally {
