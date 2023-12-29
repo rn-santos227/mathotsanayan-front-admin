ecd682bfa76f1578845d6b7c2f692640aab5ca26
rn-santos227 <rn_santos227@yahoo.com>
2023-12-29 23:14:58 +0800

    added dashboard interface

---
+++ src/interfaces/Dashboard.ts
@@ -0,0 +1,10 @@
+interface Dashboard {
+  courses: number;
+  modules: number;
+  results: number;
+  schools: number;
+  students: number;
+  subjects: number;
+  teachers: number;
+}
+export default Dashboard;
