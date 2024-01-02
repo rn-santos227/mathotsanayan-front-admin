2c1a6332786ca013beba7d7a063b670dec0063bd
rn-santos227 <rn_santos227@yahoo.com>
2024-01-02 21:43:32 +0800

    update result search list

---
+++ src/helpers/searches/result_search.ts
@@ -0,0 +1,17 @@
+import Search from "@/interfaces/Search";
+
+const search: Array<Search> = [
+  {
+    text: "Module Name",
+    value: "module.name",
+  },
+  {
+    text: "Student Name",
+    value: "student.full_name",
+  },
+  {
+    text: "Student Section",
+    value: "student.section",
+  },
+];
+export default search;
