cd3b4d4c68091915e556cc56c01be2e17a25d30c
rn-santos227 <rn_santos227@yahoo.com>
2024-01-07 17:07:51 +0800

    added list reset in modules table

--- src/views/modules/TableView.vue
+++ src/views/modules/TableView.vue
@@ -12,6 +12,17 @@
             variant="outlined"
           />
           <FilterView />
+          <v-btn
+            class="ml-4"
+            variant="outlined"
+            color="purple-darken-3"
+            prepend-icon="mdi-refresh"
+            height="40"
+            width="180"
+            @click.prevent="resetSearch"
+          >
+            Reset List
+          </v-btn>
         </v-col>
       </v-row>
     </v-card>
@@ -113,6 +124,10 @@ watch(
     await moduleModule.read(useModuleModule().currentPage);
   }
 );
+
+const resetSearch = async () => {
+  await useModuleModule().read();
+};
 
 provide("success", success);
 provide("error", error);
