96c576b9ed6086d04c0cdc24e915b5f4f414ab79
rn-santos227 <rn_santos227@yahoo.com>
2024-01-20 21:41:14 +0800

    removed password in update in students page

---
+++ src/views/audit/DetailsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-list-item @click.prevent>
+    <v-list-item-title class="text-button">
+      <v-icon icon="mdi-maagnify"></v-icon> Details
+    </v-list-item-title>
+    <v-dialog
+      class="ma-auto"
+      persistent
+      v-model="dialog"
+      activator="parent"
+      width="50%"
+    >
+    </v-dialog>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { inject, ref, reactive, computed, watch, onMounted } from "vue";
+
+const dialog = ref<boolean>(false);
+</script>
