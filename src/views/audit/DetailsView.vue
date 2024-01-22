63110500e798f79915ac50e03abbcab65681d6f7
rn-santos227 <rn_santos227@yahoo.com>
2024-01-22 22:51:24 +0800

    update audit details panel

--- src/views/audit/DetailsView.vue
+++ src/views/audit/DetailsView.vue
@@ -10,12 +10,41 @@
       activator="parent"
       width="50%"
     >
+      <v-card>
+        <v-card
+          class="rounded-0 rounded-t mb-6 py-2"
+          color="purple-darken-3"
+          flat
+        >
+          <v-card-actions class="mx-4">
+            <span class="text-h6"> Activity ID: {{ padLeft(state.id) }} </span>
+            <v-spacer />
+            <v-btn
+              density="comfortable"
+              variant="outlined"
+              icon="mdi-close"
+              @click="close"
+            >
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-card>
     </v-dialog>
   </v-list-item>
 </template>
 
 <script setup lang="ts">
-import { inject, ref, reactive, computed, watch, onMounted } from "vue";
+import { reactive, ref } from "vue";
+import { padLeft } from "@/helpers/utils";
+
+import Audit from "@/types/Audit";
 
 const dialog = ref<boolean>(false);
+const props = defineProps<{
+  audit: Audit;
+}>();
+const state = reactive<Audit>({ ...props.audit });
+const close = () => {
+  dialog.value = !dialog.value;
+};
 </script>
