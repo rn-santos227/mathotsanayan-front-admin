220021a575b2c6c288747166abe4f11cb36b627d
rn-santos227 <rn_santos227@yahoo.com>
2024-01-25 21:29:59 +0800

    added docs list for help panel

---
+++ src/helpers/docs.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    text: "Introduction",
+    page: 1,
+    subpages: [],
+  },
+  {
+    text: "Dashboard",
+    page: 2,
+    subpages: [],
+  },
+  {
+    text: "Modules",
+    page: 3,
+    subpages: [],
+  },
+  {
+    text: "Students",
+    page: 4,
+    subpages: [],
+  },
+  {
+    text: "Reports",
+    page: 5,
+    subpages: [],
+  },
+  {
+    text: "Teachers",
+    page: 6,
+    subpages: [],
+  },
+  {
+    text: "Libraries",
+    page: 7,
+    subpages: [],
+  },
+  {
+    text: "Audit Trail",
+    page: 8,
+    subpages: [],
+  },
+];
