2802bb2e2aa65eec530c275066bb355a3ebc5063
rn-santos227 <rn_santos227@yahoo.com>
2024-01-25 21:34:33 +0800

    added subpages on admin documents

--- src/helpers/docs.ts
+++ src/helpers/docs.ts
@@ -12,7 +12,52 @@ export default [
   {
     text: "Modules",
     page: 3,
-    subpages: [],
+    subpages: [
+      {
+        text: "Modules",
+        page: 1,
+      },
+      {
+        text: "Modules Creation",
+        page: 2,
+      },
+      {
+        text: "Modules Update",
+        page: 3,
+      },
+      {
+        text: "Modules Deletion",
+        page: 4,
+      },
+      {
+        text: "Questions List",
+        page: 5,
+      },
+      {
+        text: "Question Creation",
+        page: 6,
+      },
+      {
+        text: "Question Update",
+        page: 7,
+      },
+      {
+        text: "Question Delete",
+        page: 9,
+      },
+      {
+        text: "Question Test",
+        page: 10,
+      },
+      {
+        text: "Options Panel",
+        page: 11,
+      },
+      {
+        text: "Solutions Panel",
+        page: 12,
+      },
+    ],
   },
   {
     text: "Students",
