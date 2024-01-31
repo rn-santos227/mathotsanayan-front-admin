50b487dd2309e929c5b2ed1ca3fceedb9cd2c1fe
rn-santos227 <rn_santos227@yahoo.com>
2024-01-31 23:20:26 +0800

    update module page in help panel

--- src/views/help/documentation/DocModulesView.vue
+++ src/views/help/documentation/DocModulesView.vue
@@ -1,17 +1,34 @@
 <template>
   <v-card-text>
-    <span class="text-h5 font-weight-bold"> MatHotSanayan Admin Modules </span>
-    <v-divider class="my-2" />
-    <div v-if="focusPage === 'module-main'"></div>
+    <div v-if="focusPage === 'module-main'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Admin Modules Table
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'module-search'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Admin Modules Filter Search
+      </span>
+      <v-divider class="my-2" />
+    </div>
   </v-card-text>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const props = defineProps<{
   page: string;
 }>();
 
 const focusPage = ref(props.page);
+
+watch(
+  () => props.page,
+  (newValue) => {
+    focusPage.value = newValue;
+  }
+);
 </script>
