e423e8736c9fcd64511ea72d69e58e275a2a8d62
rn-santos227 <rn_santos227@yahoo.com>
2024-02-01 22:58:13 +0800

    update module page in help panel

--- src/views/help/documentation/DocModulesView.vue
+++ src/views/help/documentation/DocModulesView.vue
@@ -5,6 +5,36 @@
         MatHotSanayan Admin Modules Page
       </span>
       <v-divider class="my-2" />
+      <p class="ma-6">
+        The Modules Manager Page in MatHotSanayan acts as a central hub,
+        providing administrators with a robust set of tools to oversee and
+        manage assessment modules efficiently. This pivotal control center
+        streamlines essential tasks, offering a comprehensive suite of features
+        for creating, updating, activating, and deleting modules. Administrators
+        can effortlessly navigate through step-by-step procedures outlined in
+        this documentation, ensuring a user-friendly experience in handling the
+        intricacies of module management. From the inception of a new module to
+        its eventual deactivation or deletion, every aspect is covered,
+        empowering administrators with the control they need.
+      </p>
+      <v-img
+        class="ma-6"
+        cover
+        :src="require('@/assets/help/doc_002.png')"
+      ></v-img>
+      <p class="ma-6">
+        In addition to core module management functionalities, this
+        documentation sheds light on the seamless integration of question
+        management within the Modules Manager Page. This ensures a holistic
+        approach to assessment administration, allowing administrators to not
+        only structure modules but also add questions effortlessly. By providing
+        detailed insights into the process of question addition, [CAA Tool Name]
+        offers a comprehensive solution that enables administrators to tailor
+        assessments to their specific needs. This dual functionality,
+        encompassing both module and question management, establishes the
+        Modules Manager Page as a versatile and indispensable tool within the
+        MatHotSanayan ecosystem.
+      </p>
     </div>
 
     <div v-if="focusPage === 'module-search'">
