516d62d1dff85ca8af8d600138204922d4588d8a
rn-santos227 <rn_santos227@yahoo.com>
2024-02-01 22:39:35 +0800

    update doc reports page in help panel

--- src/views/help/documentation/DocReportsView.vue
+++ src/views/help/documentation/DocReportsView.vue
@@ -18,6 +18,27 @@
       </span>
       <v-divider class="my-2" />
     </div>
+
+    <div v-if="focusPage === 'reports-details'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Admin Report Details
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'reports-answers'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Admin Report Answer Table
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'reports-invalidate'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Admin Invalidating a Result
+      </span>
+      <v-divider class="my-2" />
+    </div>
   </v-card-text>
 </template>
 
