8aec3d332c004945152b2ee6b1a1054f33130f7e
rn-santos227 <rn_santos227@yahoo.com>
2024-02-02 22:10:41 +0800

    update module docs in help panel

---
+++ src/helpers/documentation/doc_modules.ts
@@ -0,0 +1,49 @@
+const docs: string[] = [
+  `The Modules Manager Page in MatHotSanayan acts as a central hub,
+  providing administrators with a robust set of tools to oversee and
+  manage assessment modules efficiently. This pivotal control center
+  streamlines essential tasks, offering a comprehensive suite of features
+  for creating, updating, activating, and deleting modules. Administrators
+  can effortlessly navigate through step-by-step procedures outlined in
+  this documentation, ensuring a user-friendly experience in handling the
+  intricacies of module management. From the inception of a new module to
+  its eventual deactivation or deletion, every aspect is covered,
+  empowering administrators with the control they need.`,
+
+  `In addition to core module management functionalities, this
+  documentation sheds light on the seamless integration of question
+  management within the Modules Manager Page. This ensures a holistic
+  approach to assessment administration, allowing administrators to not
+  only structure modules but also add questions effortlessly. By providing
+  detailed insights into the process of question addition, MatHotSanayan
+  offers a comprehensive solution that enables administrators to tailor
+  assessments to their specific needs. This dual functionality,
+  encompassing both module and question management, establishes the
+  Modules Manager Page as a versatile and indispensable tool within the
+  MatHotSanayan ecosystem.`,
+
+  `The Module Page Filter Search function in MatHotSanayan introduces a
+  versatile and efficient way for administrators to pinpoint specific
+  modules based on various criteria. This feature empowers users to
+  streamline their workflow by searching for modules according to three
+  key parameters: module name, subject name, or keywords within the
+  module's description.`,
+
+  `With the search by module name option, administrators can quickly locate
+  a particular module by typing its name directly into the search bar.
+  This targeted approach ensures swift access to specific assessments,
+  saving valuable time and enhancing overall navigation.`,
+
+  `Similarly, the search by subject name allows for the effortless
+  categorization of modules. Administrators can filter modules based on
+  their designated subject, facilitating a systematic organization that
+  aligns with the educational institution's curriculum structure.`,
+
+  `Moreover, the search by words within the module's description adds a
+  layer of granularity to the search process. This functionality allows
+  administrators to locate modules based on specific keywords present in
+  their descriptions, providing a more nuanced and tailored approach to
+  information retrieval.`,
+];
+
+export default docs;
--- src/views/help/documentation/DocModulesView.vue
+++ src/views/help/documentation/DocModulesView.vue
@@ -6,32 +6,13 @@
       </span>
       <v-divider class="my-2" />
       <p class="ma-6">
-        The Modules Manager Page in MatHotSanayan acts as a central hub,
-        providing administrators with a robust set of tools to oversee and
-        manage assessment modules efficiently. This pivotal control center
-        streamlines essential tasks, offering a comprehensive suite of features
-        for creating, updating, activating, and deleting modules. Administrators
-        can effortlessly navigate through step-by-step procedures outlined in
-        this documentation, ensuring a user-friendly experience in handling the
-        intricacies of module management. From the inception of a new module to
-        its eventual deactivation or deletion, every aspect is covered,
-        empowering administrators with the control they need.
+        {{ docs[0] }}
       </p>
 
       <v-img class="ma-6" cover :src="require('@/assets/help/doc_002.png')" />
 
       <p class="ma-6">
-        In addition to core module management functionalities, this
-        documentation sheds light on the seamless integration of question
-        management within the Modules Manager Page. This ensures a holistic
-        approach to assessment administration, allowing administrators to not
-        only structure modules but also add questions effortlessly. By providing
-        detailed insights into the process of question addition, MatHotSanayan
-        offers a comprehensive solution that enables administrators to tailor
-        assessments to their specific needs. This dual functionality,
-        encompassing both module and question management, establishes the
-        Modules Manager Page as a versatile and indispensable tool within the
-        MatHotSanayan ecosystem.
+        {{ docs[1] }}
       </p>
     </div>
 
@@ -41,36 +22,21 @@
       </span>
       <v-divider class="my-2" />
       <p class="ma-6">
-        The Module Page Filter Search function in MatHotSanayan introduces a
-        versatile and efficient way for administrators to pinpoint specific
-        modules based on various criteria. This feature empowers users to
-        streamline their workflow by searching for modules according to three
-        key parameters: module name, subject name, or keywords within the
-        module's description.
+        {{ docs[2] }}
       </p>
 
       <v-img class="ma-6" cover :src="require('@/assets/help/doc_004.png')" />
 
       <p class="ma-6">
-        With the search by module name option, administrators can quickly locate
-        a particular module by typing its name directly into the search bar.
-        This targeted approach ensures swift access to specific assessments,
-        saving valuable time and enhancing overall navigation.
+        {{ docs[3] }}
       </p>
 
       <p class="ma-6">
-        Similarly, the search by subject name allows for the effortless
-        categorization of modules. Administrators can filter modules based on
-        their designated subject, facilitating a systematic organization that
-        aligns with the educational institution's curriculum structure.
+        {{ docs[4] }}
       </p>
 
       <p class="ma-6">
-        Moreover, the search by words within the module's description adds a
-        layer of granularity to the search process. This functionality allows
-        administrators to locate modules based on specific keywords present in
-        their descriptions, providing a more nuanced and tailored approach to
-        information retrieval.
+        {{ docs[5] }}
       </p>
     </div>
 
@@ -330,7 +296,7 @@
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
-
+import docs from "@/helpers/documentation/doc_modules";
 const props = defineProps<{
   page: string;
 }>();
