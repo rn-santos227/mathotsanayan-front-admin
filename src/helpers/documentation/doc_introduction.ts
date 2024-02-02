74a09917aa20e440f6ade6a46dbe3c3b9eb19ce9
rn-santos227 <rn_santos227@yahoo.com>
2024-02-02 21:57:33 +0800

    created separated paragraphs for documentation

---
+++ src/helpers/documentation/doc_introduction.ts
@@ -0,0 +1,24 @@
+const docs: string[] = [
+  `Welcome to MatHotSanayan Administration, your comprehensive platform for proficiently managing Computer Aided Assessment (CAA). In this guide, we aim to illuminate the key functionalities that empower administrators to oversee assessments seamlessly. The admin dashboard serves as the nerve center, offering a bird's-eye view of ongoing assessments, upcoming exams, and recent results. With an intuitive interface, administrators can effortlessly navigate through vital performance indicators, ensuring informed decision-making at every step.`,
+
+  `In the realm of Modules Management, MatHotSanayan provides administrators
+  with the tools to create, customize, and monitor assessment modules
+  efficiently. From defining exam parameters to scheduling assessments, the
+  system ensures a streamlined process, giving administrators full control
+  over the examination schedule. This section guides administrators through
+  the process of setting up modules, managing timelines, and monitoring
+  ongoing assessments, enabling them to orchestrate a cohesive and organized
+  assessment environment.`,
+
+  `Student Management is a pivotal feature, facilitating administrators in
+  enrolling students seamlessly into the system and managing their
+  information effortlessly. With MatHotSanayan, administrators can customize
+  student profiles, ensuring accurate and up-to-date records. The platform's
+  user-friendly interface for students enhances the overall experience,
+  offering access to relevant study materials and a secure testing
+  environment. This guide walks administrators through the process of adding
+  and updating student information, ensuring that the student management
+  system aligns seamlessly with the institution's needs.`,
+];
+
+export default docs;
