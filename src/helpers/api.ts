edeb746eb86a86351a0a090a6b49bb64c7251ea9
rn-santos227 <rn_santos227@yahoo.com>
2024-02-04 19:15:47 +0800

    update api helper

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -72,6 +72,7 @@ export default {
     CREATE: `${BACKEND}questions/create`,
     CREATEALL: `${BACKEND}questions/create-all/`,
     READ: `${BACKEND}questions`,
+    REMOVEIMG: `${BACKEND}questions/questions/img-remove/`,
     UPDATE: `${BACKEND}questions/`,
     DELETE: `${BACKEND}questions/`,
   },
