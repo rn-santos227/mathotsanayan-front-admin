1eb5c066ff16c65b752c66cc8e4dec24d2abb45f
rn-santos227 <rn_santos227@yahoo.com>
2024-02-08 23:26:49 +0800

    update report page in help panel

--- src/helpers/documentation/doc_reports.ts
+++ src/helpers/documentation/doc_reports.ts
@@ -1,3 +1,31 @@
-const docs: string[] = [];
+const docs: string[] = [
+  `Effectively manage and analyze student exam results within
+  MatHotSanayan's Module/Exam Result Manager, providing administrators
+  with comprehensive insights into student performance. This robust
+  functionality allows administrators to view detailed exam results,
+  including students' scores, grades, and the time taken to answer each
+  question. Additionally, administrators can access information on the
+  number of attempts made by students to answer each question correctly,
+  providing valuable insights into individual learning strategies and
+  areas for improvement.`,
+
+  `Furthermore, the Module/Exam Result Manager displays a list of all
+  answers provided by students, offering a comprehensive overview of their
+  responses throughout the exam. Administrators can easily identify any
+  patterns or trends in students' answers, facilitating targeted
+  interventions and personalized support where necessary. Additionally,
+  the manager highlights the number of questions skipped by students,
+  aiding administrators in understanding student engagement and potential
+  areas of difficulty.`,
+
+  `As part of its comprehensive functionality, the Module/Exam Result
+  Manager empowers administrators to invalidate exam results if necessary.
+  This feature provides administrators with the flexibility to address any
+  anomalies or discrepancies in student performance, ensuring the
+  integrity and accuracy of exam data. By offering a range of tools for
+  result analysis and management, MatHotSanayan's Module/Exam Result
+  Manager enhances administrators' ability to support student learning and
+  drive academic success effectively.`,
+];
 
 export default docs;
