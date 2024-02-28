a2537dd4bd60d74ab270cfbb8fa55cb6b8598188
rn-santos227 <rn_santos227@yahoo.com>
2024-02-28 23:25:44 +0800

    fixed subject in result search

---
+++ src/helpers/searches/search_results.ts
@@ -0,0 +1,29 @@
+import Category from "@/interfaces/Category";
+
+const search_list: Array<Category> = [
+  {
+    text: "Module Name",
+    value: "module.name",
+  },
+  {
+    text: "Student Name",
+    value: "student.full_name",
+  },
+  {
+    text: "Course",
+    value: "student.course",
+  },
+  {
+    text: "School",
+    value: "student.school",
+  },
+  {
+    text: "Section",
+    value: "student.section",
+  },
+  {
+    text: "Subject",
+    value: "module.subject",
+  },
+];
+export default search_list;
