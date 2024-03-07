7f05d3d3529a2ecf7090a28b5b5d3956e5905a12
rn-santos227 <rn_santos227@yahoo.com>
2024-03-07 22:14:58 +0800

    update module update panel

--- src/views/modules/UpdateView.vue
+++ src/views/modules/UpdateView.vue
@@ -47,7 +47,7 @@
                 <v-select
                   :items="steps"
                   v-model.trim="v$.step.$model"
-                  label="Module Step"
+                  label="Module Stage"
                   density="compact"
                   variant="outlined"
                   :error="v$.step.$error"
@@ -71,7 +71,7 @@
                 <v-autocomplete
                   v-model.trim="v$.subject.$model"
                   prepend-inner-icon="mdi-book"
-                  label="Module Subject"
+                  label="Subject"
                   density="compact"
                   variant="outlined"
                   item-title="name"
@@ -87,7 +87,7 @@
                 <v-textarea
                   rows="3"
                   v-model.trim="state.description"
-                  label="Module Description"
+                  label="Description"
                   density="compact"
                   variant="outlined"
                 />
@@ -98,7 +98,7 @@
                 <v-textarea
                   rows="3"
                   v-model.trim="state.objective"
-                  label="Module Objective"
+                  label="Objective"
                   density="compact"
                   variant="outlined"
                 />
@@ -109,7 +109,7 @@
                 <v-textarea
                   rows="3"
                   v-model.trim="state.direction"
-                  label="Module Direction/Instruction"
+                  label="Instructions"
                   density="compact"
                   variant="outlined"
                 />
