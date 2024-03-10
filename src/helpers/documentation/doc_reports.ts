969bc7a57743c4773f1b0d7361811e766b9ee0b7
rn-santos227 <rn_santos227@yahoo.com>
2024-03-10 22:28:51 +0800

    jpdate reports details page in help panel

--- src/helpers/documentation/doc_reports.ts
+++ src/helpers/documentation/doc_reports.ts
@@ -1,25 +1,25 @@
 const docs: string[] = [
-  `        In MatHOTSanayan's Examination Results Page, administrators can easily
+  `In MatHOTSanayan's Examination Results Page, administrators can easily
   manage and analyze student's exam scores. This gives them a deep
   understanding of how students are doing. They can view detailed exam
   results like scores, remarks, and how long students took to answer each
   module.`,
 
-  `        Moreover, the Examination Results Page has action buttons that let the
+  `Moreover, the Examination Results Page has action buttons that let the
   administrators see more information about the exam results and
   invalidate them if needed. The "Details" button shows administrators
   extra details like how many times a question was attempted and how many
   questions were skipped. On the other hand, the "Invalidate" button lets
   administrators invalidate exam results if there is a reason to do so.`,
 
-  `        The Examination Results Page's search functionality helps administrators
+  `The Examination Results Page's search functionality helps administrators
   quickly find results using different criteria, making it easy to get the
   information they need. They can search by module title, student name,
   course, school, section, or subject. Just by typing in keywords or
   details, administrators can find exactly what they are looking for,
   saving time and making their work more efficient.`,
 
-  `        This flexible feature lets administrators customize their searches to
+  `This flexible feature lets administrators customize their searches to
   fit their needs, making it easier to find and organize information in
   the Examination Results Page. Whether they are looking for results for a
   specific module, student, course, school, section, or subject,
@@ -27,7 +27,7 @@ const docs: string[] = [
   gives them many options to search, so they can manage exam results
   effectively and keep track of student progress.`,
 
-  `        In MatHOTSanayan, administrators can effortlessly create detailed Excel
+  `In MatHOTSanayan, administrators can effortlessly create detailed Excel
   reports using the Advanced Filter feature. This feature helps them
   extract precise data based on their search criteria, ensuring that the
   Excel file contains exactly the information they need. This reports
@@ -35,20 +35,20 @@ const docs: string[] = [
   module stage, score, remarks, and total time taken by students to finish
   a module.`,
 
-  `        To start generating a report, administrators first refine their dataset
+  `To start generating a report, administrators first refine their dataset
   by conducting a search using the Advanced Filter feature. They can
   filter results by student names, sections, module titles, or any other
   relevant criteria. Once satisfied with the filtered dataset, they can
   export the data into an Excel file by clicking the “Download Excel”
   button.`,
 
-  `        The generated Excel report provides a comprehensive overview of student
+  `The generated Excel report provides a comprehensive overview of student
   performance, allowing administrators to analyze key metrics such as
   scores and completion times. This valuable information can be utilized
   for various purposes, including academic assessments, performance
   evaluations, and progress tracking.`,
 
-  `        The Results Details section gives administrators a thorough look at
+  `The Results Details section gives administrators a thorough look at
   students' exam performance, helping them understand what students are
   good at and where they need to improve. It starts by showing important
   information like the student's name, section, the module title
@@ -57,12 +57,28 @@ const docs: string[] = [
   finish the exam and get an idea of how much time they spent on each
   question.`,
 
-  `        In addition to qualitative insights, the Results Details section
+  `In addition to qualitative insights, the Results Details section
   provides numbers that give a clear idea of how well students understand
   the subject. These numbers show their scores compared to the total
   number of questions, giving a measure of their skills. The percentage
   grade shows how well they did according to the grading system, helping
   administrators know if they met expectations.`,
+
+  `Administrators can also see how engaged students were by looking at how
+  many times they attempted each question. The accuracy score, which looks
+  at the questions they answered correctly compared to how many they
+  tried, along with the percentage grade, gives a good overall picture of
+  performance, considering both accuracy and persistence. And by seeing
+  how many questions students skipped, administrators can understand how
+  they made decisions during the exam.`,
+
+  `The third segment of the Results Details section presents a detailed
+  table showing each question alongside the student's responses. This
+  detailed view helps administrators go through responses one by one,
+  identify recurring patterns, and understand how students solve problems.
+  By looking closely at individual responses, administrators can figure
+  out where students are strong and where they need help, making it easier
+  to plan ways to improve their performance in future exams.`,
 ];
 
 export default docs;
