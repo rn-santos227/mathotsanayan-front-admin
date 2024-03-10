5e4a3524445a66198d1d5360c44bdf2d85d3a915
rn-santos227 <rn_santos227@yahoo.com>
2024-03-10 22:48:15 +0800

    update teachers manager page in help panel

--- src/helpers/documentation/doc_teachers.ts
+++ src/helpers/documentation/doc_teachers.ts
@@ -6,6 +6,13 @@ const docs: string[] = [
   interface. Its primary purpose is to make handling teacher information
   smooth, so adding new teachers or updating existing ones is effortless
   for administrators.`,
+
+  `One of the best features of this page is how easy it is to use. Whether
+  administrators are adding new teachers or updating existing information,
+  the interface is designed to simplify these processes, reducing the time
+  and effort required for such tasks. This simplicity ensures that
+  administrators can navigate the system with ease, regardless of their
+  level of technical expertise.`
 ];
 
 export default docs;
