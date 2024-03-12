f08a71b97eb87ee34de50ef01fe7bca4a0d1bb21
rn-santos227 <rn_santos227@yahoo.com>
2024-03-12 21:47:54 +0800

    update auidt page in help panel

--- src/helpers/documentation/doc_audit.ts
+++ src/helpers/documentation/doc_audit.ts
@@ -6,6 +6,13 @@ const docs: string[] = [
   resource, and timestamp. The account name is the username associated
   with the individual performing the activity, helping to trace actions
   back to specific users.`,
+
+  `Activities within the audit trail encompass a wide array of actions,
+  ranging from user logins, creation of new students or teachers, updates
+  to existing courses or schools, deletion of subjects, to the initiation
+  or completion of examinations. This detailed information shows every
+  user interaction with the system, providing invaluable insight into
+  system usage and behavior.`,
 ];
 
 export default docs;
