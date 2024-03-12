0e4993546140c103ede158f31d0e435a19c5fad4
rn-santos227 <rn_santos227@yahoo.com>
2024-03-12 21:47:16 +0800

    update auidt page in help panel

--- src/views/help/documentation/DocAuditView.vue
+++ src/views/help/documentation/DocAuditView.vue
@@ -8,6 +8,18 @@
       <p class="ma-6">
         {{ docs[0] }}
       </p>
+
+      <p class="ma-6">
+        {{ docs[1] }}
+      </p>
+
+      <p class="ma-6">
+        {{ docs[2] }}
+      </p>
+
+      <p class="ma-6">
+        {{ docs[3] }}
+      </p>
     </div>
 
     <div v-if="focusPage === 'audit-search'">
