fba98a6ca5b1c766507340b02f616dbdd89e7dc3
rn-santos227 <rn_santos227@yahoo.com>
2024-03-12 21:48:47 +0800

    update audit page in help panel

--- src/helpers/documentation/doc_audit.ts
+++ src/helpers/documentation/doc_audit.ts
@@ -13,6 +13,12 @@ const docs: string[] = [
   or completion of examinations. This detailed information shows every
   user interaction with the system, providing invaluable insight into
   system usage and behavior.`,
+
+  `Complementing the activity log is the resource field, which specifies
+  the resource affected by the activity. Whether it involves users,
+  modules, questions, courses, or other system components, this attribute
+  adds depth to the audit trail, offering clarity on the scope and context
+  of each activity within system interactions.`,
 ];
 
 export default docs;
