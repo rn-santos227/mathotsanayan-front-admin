ec4e9baaae21e85b35959d8f21b3c59258057eae
rn-santos227 <rn_santos227@yahoo.com>
2024-03-12 21:44:25 +0800

    update subject update panel

--- src/views/subjects/UpdateView.vue
+++ src/views/subjects/UpdateView.vue
@@ -36,7 +36,7 @@
               <v-col>
                 <v-text-field
                   v-model.trim="v$.name.$model"
-                  label="Subject Name"
+                  label="Subject Title"
                   density="compact"
                   variant="outlined"
                   :error="v$.name.$error"
