8918a667bff5c42ff50ba6061cb29b949528067d
rn-santos227 <rn_santos227@yahoo.com>
2024-03-12 21:46:52 +0800

    update auidt page in help panel

--- src/helpers/documentation/doc_audit.ts
+++ src/helpers/documentation/doc_audit.ts
@@ -1,3 +1,11 @@
-const docs: string[] = [];
+const docs: string[] = [
+  `The audit trail feature of MatHOTSanayan serves as a detailed
+  record-keeper, documenting all activities and operations performed
+  within the system, providing transparency, accountability, and security.
+  It displays essential information, including the account name, activity,
+  resource, and timestamp. The account name is the username associated
+  with the individual performing the activity, helping to trace actions
+  back to specific users.`,
+];
 
 export default docs;
